572239c3d775a3ca486d28386636f23480c5e31f
israel henrique castro pontes <israelpontes@MacBook-Pro-de-israel.local>
2022-11-03 11:56:28 -0300

    feat(GET/teweets)implementei a rota tweets que retorna com os 10 ultimos tweets

--- index.js
+++ index.js
@@ -2,6 +2,15 @@ import express from "express";
 
 const app = express();
 
+const usuarios = [];
+const tweets = ['teste1','teste2','teste3','teste4','teste5','teste6','teste7','teste8','teste9','teste10',];
+
+app.get('/tweets', (req, res) => {
+    const lestTweets = tweets.slice(-10);
+    res.send(lestTweets);
+})
+
 app.listen(5001, () => {
     console.log('Ouvindo na porta 5001')
 })
+
