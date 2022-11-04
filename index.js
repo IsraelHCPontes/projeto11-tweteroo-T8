0612372edc8d12bfd3813f802f443495c29ea642
israel henrique castro pontes <israelpontes@MacBook-Pro-de-israel.local>
2022-11-03 22:41:52 -0300

    feat([GET/teweets, POST/sing-up, POST/tweets])implementei as rotas de cadatros e post de tweets e add a cada tweet enviado pelo get o avatar correspondente

--- index.js
+++ index.js
@@ -1,16 +1,31 @@
 import express from "express";
 
 const app = express();
+app.use(express.json());
 
 const usuarios = [];
-const tweets = ['teste1','teste2','teste3','teste4','teste5','teste6','teste7','teste8','teste9','teste10',];
+const tweets = [];
 
-app.get('/tweets', (req, res) => {
+app.post("/sing-up", (req, res) => {
+    const usuario = req.body;
+    usuarios.push(usuario);
+    res.send("ok");
+});
+
+app.post("/tweets", (req, res) => {
+    const tweet = req.body;
+    tweets.push(tweet);
+    res.send("ok");
+})
+
+app.get("/tweets", (req, res) => {
+    tweets.forEach(tweet => {
+        const {avatar} = usuarios.find(usuario => tweet.username === usuario.username);
+        tweet.avatar = avatar;
+    })
     const lestTweets = tweets.slice(-10);
     res.send(lestTweets);
-})
+});
 
-app.listen(5001, () => {
-    console.log('Ouvindo na porta 5001')
-})
+app.listen(5001, () => console.log('Ouvindo na porta 5001'));
 
